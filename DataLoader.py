01ab22b38abcf3bd083bcea85de999e890553825
klemmedan <klemmedan@gmail.com>
2020-07-05 16:30:13 -0500

    Optimizer works with basic production/demand upgrades. Also created the PowerValue class to multiply income directly.
    Next steps: Implement PowerValue into score calculations, as well as Multi Upgrades/Unlocks and PowerValue Upgrades. After that, the prestige system will be added.

---
+++ DataLoader.py
@@ -0,0 +1,206 @@
+
+import openpyxl
+from IdlePowerOptimization import Optimizer, UpgradeManager, Resource
+from typing import List, Optional
+
+
+class DataLoader:
+
+    def __init__(self):
+        self.production_resources = list()  # type: List[Resource]
+        self.demand_resources = list()  # type: List[Resource]
+        self.production_upgrade_managers = list()  # type: List[UpgradeManager]
+        self.demand_upgrade_managers = list()  # type: List[UpgradeManager]
+        self.production_multi_unlock_factors = list()
+        self.production_multi_unlock_thresholds = list()
+        self.production_multi_upgrade_manager = UpgradeManager()
+        self.demand_multi_unlock_factors = list()
+        self.demand_multi_unlock_thresholds = list()
+        self.demand_multi_upgrade_manager = UpgradeManager()
+        self.optimizer = Optimizer()
+
+        self.wb = openpyxl.load_workbook(filename="IdlePowerBalancing.xlsx", data_only=True)
+        self.prod_sheet_name = 'prod_base_values'
+        self.prod_unlock_sheet_name = 'prod_single_unlocks'
+        self.prod_upgrade_sheet_name = 'prod_single_upgrades'
+        self.prod_multi_sheet_name = 'prod_multi'
+
+        self.demand_sheet_name = 'demand_base_values'
+        self.demand_unlock_sheet_name = 'demand_single_unlocks'
+        self.demand_upgrade_sheet_name = 'demand_single_upgrades'
+        self.demand_manual_unlocks_sheet_name = 'demand_manual_unlocks'
+        self.demand_manual_upgrades_sheet_name = 'demand_manual_upgrades'
+        self.demand_multi_sheet_name = 'demand_multi'
+
+    def load_optimizer(self):
+        # type: () -> Optimizer
+        self.production_resources = self.load_prod_resources()
+        self.demand_resources = self.load_demand_resources()
+        self.production_upgrade_managers = self.load_prod_upgrade_managers()
+        self.demand_upgrade_managers = self.load_demand_upgrade_managers()
+
+        self.production_multi_upgrade_manager = self.load_multi_prod_upgrade_manager()
+        self.production_multi_unlock_thresholds, self.production_multi_unlock_factors = self.load_multi_prod_unlocks()
+        self.demand_multi_upgrade_manager = self.load_multi_demand_upgrade_manager()
+        self.demand_multi_unlock_thresholds, self.demand_multi_unlock_factors = self.load_multi_demand_unlocks()
+
+        optimizer = Optimizer()
+        optimizer.prod_resources = self.production_resources
+        optimizer.demand_resources = self.demand_resources
+        optimizer.prodMultiUnlockFactors = self.production_multi_unlock_factors
+        optimizer.prodMultiUnlockThresholds = self.production_multi_unlock_thresholds
+        optimizer.prodMultiUpgrades = self.production_multi_upgrade_manager
+        optimizer.prod_upgrade_managers = self.production_upgrade_managers
+        optimizer.demandMultiUnlockFactors = self.demand_multi_unlock_factors
+        optimizer.demandMultiUnlockThresholds = self.demand_multi_unlock_thresholds
+        optimizer.demand_upgrade_managers = self.demand_upgrade_managers
+        optimizer.demandMultiUpgrades = self.demand_multi_upgrade_manager
+
+        return optimizer
+
+    def load_prod_resources(self):
+        ws = self.wb[self.prod_sheet_name]
+        index = 0
+        resources = list()
+        for row in ws.iter_rows(min_row=3, min_col=2, max_col=4):
+            if row[0].value is None:
+                break
+            base_increment = row[0].value
+            base_cost = row[1].value
+            growth_rate = row[2].value
+            thresholds, factors = self.load_prod_unlocks(index)
+            resource = Resource()
+            if index == 0:
+                resource.amount = 1
+            resource.base_increment = base_increment
+            resource.base_cost = base_cost
+            resource.growth_rate = growth_rate
+            resource.unlock_thresholds = thresholds
+            resource.unlock_factors = factors
+            resources.append(resource)
+            index += 1
+        return resources
+
+    def load_prod_unlocks(self, index):
+        ws = self.wb[self.prod_unlock_sheet_name]
+        thresholds = [0] + [row[0].value for row in ws.iter_rows(min_row=3)]
+        base_factors = [row[index+1].value for row in ws.iter_rows(min_row=3)]
+        factors = self.cumulative_factors(base_factors)
+        return thresholds, factors
+
+    def load_demand_resources(self):
+        ws = self.wb[self.demand_sheet_name]
+        index = 0
+        resources = list()
+        for row in ws.iter_rows(min_row=4, min_col=2):
+            if row[2].value is None:
+                break
+            base_increment = row[0].value
+            base_cost = row[1].value
+            growth_rate = row[2].value
+            thresholds, factors = self.load_demand_unlocks(index)
+            resource = Resource()
+            if index == 0:
+                resource.amount = 1
+            resource.base_increment = base_increment
+            resource.base_cost = base_cost
+            resource.growth_rate = growth_rate
+            resource.unlock_thresholds = thresholds
+            resource.unlock_factors = factors
+            resources.append(resource)
+            index += 1
+        return resources
+
+    def load_demand_unlocks(self, index):
+        if index == 0:
+            return self.load_manual_unlocks()
+        else:
+            ws = self.wb[self.demand_unlock_sheet_name]
+            thresholds = [0] + [row[0].value for row in ws.iter_rows(min_row=4)]
+            base_factors = [row[index].value for row in ws.iter_rows(min_row=4)]
+            factors = self.cumulative_factors(base_factors)
+            return thresholds, factors
+
+    def load_manual_unlocks(self):
+        ws = self.wb[self.demand_manual_unlocks_sheet_name]
+        thresholds = [0] + [row[0].value for row in ws.iter_rows(min_row=4)]
+        base_factors = [row[1].value for row in ws.iter_rows(min_row=4)]
+        factors = self.cumulative_factors(base_factors)
+        return thresholds, factors
+
+    def load_prod_upgrade_managers(self):
+        num_resources = len(self.production_resources)
+        return [self.load_prod_upgrade_manager(i) for i in range(num_resources)]
+
+    def load_prod_upgrade_manager(self, index):
+        ws = self.wb[self.prod_upgrade_sheet_name]
+        costs = [0] + [row[index].value for row in ws.iter_rows(min_row=3, min_col=2)]
+        base_factors = [row[index+int(len(row)/2)].value for row in ws.iter_rows(min_row=3, min_col=2)]
+        factors = self.cumulative_factors(base_factors)
+
+        return self.make_upgrade_manager(costs, factors)
+
+    def load_demand_upgrade_managers(self):
+        num_resources = len(self.demand_resources)
+        return [self.load_demand_upgrade_manager(i) for i in range(num_resources)]
+
+    def load_demand_upgrade_manager(self, index):
+        if index == 0:
+            return self.load_manual_upgrade_manager()
+        else:
+            ws = self.wb[self.demand_upgrade_sheet_name]
+            costs = [0] + [row[index-1].value for row in ws.iter_rows(min_row=4, min_col=2)]
+            base_factors = [row[index+int(len(row)/2-1)].value for row in ws.iter_rows(min_row=4, min_col=2)]
+            factors = self.cumulative_factors(base_factors)
+
+            return self.make_upgrade_manager(costs, factors)
+
+    def load_manual_upgrade_manager(self):
+        ws = self.wb[self.demand_manual_upgrades_sheet_name]
+        costs = [0] + [row[0].value for row in ws.iter_rows(min_row=4)]
+        base_factors = [row[1].value for row in ws.iter_rows(min_row=4)]
+        factors = self.cumulative_factors(base_factors)
+        return self.make_upgrade_manager(costs, factors)
+
+    def load_multi_prod_unlocks(self):
+        ws = self.wb[self.prod_multi_sheet_name]
+        thresholds = [0] + [row[0].value for row in ws.iter_rows(min_row=3) if row[0].value is not None]
+        base_factors = [row[1].value for row in ws.iter_rows(min_row=3) if row[0].value is not None]
+        factors = self.cumulative_factors(base_factors)
+        return thresholds, factors
+
+    def load_multi_prod_upgrade_manager(self):
+        ws = self.wb[self.prod_multi_sheet_name]
+        costs = [0] + [row[3].value for row in ws.iter_rows(min_row=3)]
+        base_factors = [row[4].value for row in ws.iter_rows(min_row=3)]
+        factors = self.cumulative_factors(base_factors)
+        return self.make_upgrade_manager(costs, factors)
+
+    def load_multi_demand_unlocks(self):
+        ws = self.wb[self.demand_multi_sheet_name]
+        thresholds = [0] + [row[0].value for row in ws.iter_rows(min_row=4) if row[0].value is not None]
+        base_factors = [row[1].value for row in ws.iter_rows(min_row=4)if row[0].value is not None]
+        factors = self.cumulative_factors(base_factors)
+        return thresholds, factors
+
+    def load_multi_demand_upgrade_manager(self):
+        ws = self.wb[self.demand_multi_sheet_name]
+        costs = [0] + [row[2].value for row in ws.iter_rows(min_row=4)]
+        base_factors = [row[3].value for row in ws.iter_rows(min_row=4)]
+        factors = self.cumulative_factors(base_factors)
+        return self.make_upgrade_manager(costs, factors)
+
+    @staticmethod
+    def cumulative_factors(base_factors):
+        factors = [1]
+        for f in base_factors:
+            factors.append(factors[-1]*f)
+        return factors
+
+    @staticmethod
+    def make_upgrade_manager(costs, factors):
+        manager = UpgradeManager()
+        manager.upgrade_costs = costs
+        manager.upgrade_factors = factors
+        return manager
+
--- IdlePowerOptimization.py
+++ IdlePowerOptimization.py
@@ -2,163 +2,87 @@ import matplotlib.pyplot as plt
 import numpy
 import math
 import copy
-import openpyxl
 from typing import List, Optional
+import DataLoader
 
-purchase_factor = 1.2
-
-
-class DataLoader:
-
-    def __init__(self):
-        self.wb = openpyxl.load_workbook(filename = "C:\\Users\\Dan\\Documents\\idle_power_balancing\\IdlePowerBalancing.xlsx")
-        self.prod_sheet_name = 'prod_base_values'
-        self.prod_unlock_sheet_name = 'prod_single_unlocks'
-        self.prod_upgrade_sheet_name = 'prod_single_upgrades'
-
-        self.demand_sheet_name = 'demand_base_values'
-        self.demand_unlock_sheet_name = 'demand_single_unlocks'
-        self.demand_upgrade_sheet_name = 'demand_single_upgrades'
-
-    def load_optimizer(self):
-        production_resources = self.load_prod_resources()
-        demand_resources = self.load_demand_resources()
-
-    def load_prod_resources(self):
-        ws = self.wb[self.prod_sheet_name]
-        index = 0
-        resources = list()
-        for row in ws.iter_rows(min_row=3, min_col=2, max_col=4):
-            if row[0].value is None:
-                break
-            base_increment = row[0].value
-            base_cost = row[1].value
-            growth_rate = row[2].value
-            thresholds, factors = self.load_prod_unlocks(index)
-            resource = Resource()
-            if index == 0:
-                resource.amount = 1
-            resource.base_increment = base_increment
-            resource.base_cost = base_cost
-            resource.growth_rate = growth_rate
-            resource.unlock_thresholds = thresholds
-            resource.unlock_factors = factors
-            resources.append(resource)
-            index += 1
-        return resources
-
-    def load_prod_unlocks(self, index):
-        thresholds = list()
-        factors = list()
-        ws = self.wb[self.prod_unlock_sheet_name]
-        for row in ws.iter_rows(min_row=3):
-            threshold = row[0].value
-            factor = row[index + 1].value
-            thresholds.append(threshold)
-            factors.append(factor)
-            print(threshold, factor)
-        return thresholds, factors
-
-    def load_demand_resources(self):
-        ws = self.wb[self.demand_sheet_name]
-        index = 0
-        resources = list()
-        for row in ws.iter_rows(min_row=3, min_col=1, max_col=3):
-            if row[0].value is None:
-                break
-            base_increment = row[0].value
-            base_cost = row[1].value
-            growth_rate = row[2].value
-            thresholds, factors = self.load_demand_unlocks(index)
-            resource = Resource()
-            if index == 0:
-                resource.amount = 1
-            resource.base_increment = base_increment
-            resource.base_cost = base_cost
-            resource.growth_rate = growth_rate
-            resource.unlock_thresholds = thresholds
-            resource.unlock_factors = factors
-            resources.append(resource)
-            index += 1
-        return resources
-
-    def load_demand_unlocks(self, index):
-        thresholds = list()
-        factors = list()
-        ws = self.wb[self.prod_unlock_sheet_name]
-        for row in ws.iter_rows(min_row=3):
-            threshold = row[0].value
-            factor = row[index + 1].value
-            thresholds.append(threshold)
-            factors.append(factor)
-            print(threshold, factor)
-        return thresholds, factors
+purchase_factor = 1.05
 
 
 class Optimizer:
 
     def __init__(self):
-        self.time = list()  # type: List[float]
-        self.demand = list()  # type: List[float]
-        self.production = list()  # type: List[float]
-        self.powerValue = list()  # type: List[float]
+        self.stat_tracker = StatTracker()
+        self.power_value = list()  # type: List[float]
         self.income = list()  # type: List[float]
         self.cumulativeProduction = list()  # type: List[float]
         self.cumulativeIncome = list()  # type: List[float]
 
-        self.productionResources = list()  # type: List[Resource]
-        self.demandResources = list()  # type: List[Resource]
+        self.prod_resources = list()  # type: List[Resource]
+        self.demand_resources = list()  # type: List[Resource]
         self.prodMultiUnlockFactors = list()  # type: List[float]
         self.prodMultiUnlockThresholds = list()  # type: List[float]
         self.demandMultiUnlockFactors = list()  # type: List[float]
         self.demandMultiUnlockThresholds = list()  # type: List[float]
         self.prodMultiUpgrades = UpgradeManager()
         self.demandMultiUpgrades = UpgradeManager()
-        self.prodUpgradeManagers = list()  # type: List[UpgradeManager]
-        self.demandUpgradeManagers = list()  # type: List[UpgradeManager]
+        self.prod_upgrade_managers = list()  # type: List[UpgradeManager]
+        self.demand_upgrade_managers = list()  # type: List[UpgradeManager]
 
-    def run_optimization(self):
-        for i in range(10000):
+    def run_optimization(self, num_steps):
+        for i in range(num_steps):
             prod_income = self.total_prod_income()
             demand_income = self.total_demand_income()
-            if prod_income > demand_income:
+            if prod_income < demand_income:
                 self.improve_prod()
             else:
                 self.improve_demand()
+
+            self.stat_tracker.update_stats(self)
 
     def improve_prod(self):
         (count_index, count_score) = self.max_prod_resource_score()
         (upgrade_index, upgrade_score) = self.max_prod_upgrade_score()
         if upgrade_score >= count_score:
-            self.prodUpgradeManagers[upgrade_index].make_purchase()
+            self.stat_tracker.update_time(self.prod_upgrade_managers[upgrade_index].current_cost(), self)
+            self.prod_upgrade_managers[upgrade_index].make_purchase()
         else:
-            self.productionResources[count_index].make_purchase()
+            self.stat_tracker.update_time(self.prod_resources[count_index].cost(), self)
+            self.prod_resources[count_index].make_purchase()
 
     def improve_demand(self):
         (count_index, count_score) = self.max_demand_resource_score()
         (upgrade_index, upgrade_score) = self.max_demand_upgrade_score()
         if upgrade_score >= count_score:
-            self.demandUpgradeManagers[upgrade_index].make_purchase()
+            self.stat_tracker.update_time(self.demand_upgrade_managers[upgrade_index].current_cost(), self)
+            self.demand_upgrade_managers[upgrade_index].make_purchase()
         else:
-            self.demandResources[count_index].make_purchase()
+            self.stat_tracker.update_time(self.demand_resources[count_index].cost(), self)
+            self.demand_resources[count_index].make_purchase()
 
     def total_prod_income(self):
         total = 0
-        for resource in self.productionResources:
-            total = total + resource.increment()
+        index = 0
+        for resource in self.prod_resources:
+            total = total + resource.increment() * self.prod_upgrade_managers[index].current_factor()
+            index += 1
         return total
 
     def total_demand_income(self):
         total = 0
-        for resource in self.productionResources:
-            total = total + resource.increment()
+        index = 0
+        for resource in self.demand_resources:
+            total = total + resource.increment() * self.demand_upgrade_managers[index].current_factor()
+            index += 1
         return total
+
+    def total_income(self):
+        return min(self.total_demand_income(), self.total_prod_income())
 
     def max_prod_resource_score(self):
         index = 0
         max_score = 0
         max_index = index
-        for resource in self.productionResources:
+        for resource in self.prod_resources:
             cost = resource.cost()
             current_increment_value = resource.increment()
             increment_value_if_purchased = resource.increment_if_purchased()
@@ -175,8 +99,8 @@ class Optimizer:
         max_index = 0
         max_score = 0
 
-        for upgrade_manager in self.prodUpgradeManagers:
-            resource_increment = self.productionResources[index].increment()
+        for upgrade_manager in self.prod_upgrade_managers:
+            resource_increment = self.prod_resources[index].increment()
             cost = upgrade_manager.next_cost()
             current_factor = upgrade_manager.current_factor()
             next_factor = upgrade_manager.next_factor()
@@ -191,7 +115,7 @@ class Optimizer:
         index = 0
         max_score = 0
         max_index = index
-        for resource in self.demandResources:
+        for resource in self.demand_resources:
             cost = resource.cost()
             current_increment_value = resource.increment()
             increment_value_if_purchased = resource.increment_if_purchased()
@@ -208,8 +132,8 @@ class Optimizer:
         max_index = 0
         max_score = 0
 
-        for upgrade_manager in self.demandUpgradeManagers:
-            resource_increment = self.demandResources[index].increment()
+        for upgrade_manager in self.demand_upgrade_managers:
+            resource_increment = self.demand_resources[index].increment()
             cost = upgrade_manager.next_cost()
             current_factor = upgrade_manager.current_factor()
             next_factor = upgrade_manager.next_factor()
@@ -219,6 +143,38 @@ class Optimizer:
                 max_index = index
             index = index + 1
         return max_index, max_score
+
+
+class PowerValue:
+
+    def __init__(self):
+        self.amount_growth_rate = 1
+        self.amount_second_growth_rate = 0.1
+        self.count = 0
+        self.base_cost = 50000
+        self.cost_growth_rate = 1.2
+
+    def factor(self):
+        return 1 + self.count*self.amount_growth_rate + self.count*(self.count - 1)*self.amount_second_growth_rate/2
+
+    def cost(self):
+        n = self.num_to_purchase()
+        total_cost = self.cost_growth_rate * (1 - self.cost_growth_rate ** (n + self.count - 1)) / (1 - self.cost_growth_rate)
+        previous_cost = self.cost_growth_rate * (1 - self.cost_growth_rate ** (self.count - 1)) / (1 - self.cost_growth_rate)
+        diff_cost = self.base_cost * (total_cost - previous_cost)
+        return diff_cost
+
+    def num_to_purchase(self):
+        base_increase = math.floor(self.count*(purchase_factor - 1))
+        return max(1, base_increase)
+
+    def factor_if_purchased(self):
+        bought_resource = copy.copy(self)
+        bought_resource.count = bought_resource.count + self.num_to_purchase()
+        return bought_resource.factor()
+
+    def make_purchase(self):
+        self.count = self.count + self.num_to_purchase()
 
 
 class Resource:
@@ -240,13 +196,13 @@ class Resource:
 
     def cost(self):
         n = self.num_to_purchase()
-        total_cost = self.growth_rate * (1 - self.growth_rate ** (n + self.amount)) / (1 - self.growth_rate)
-        previous_cost = self.growth_rate * (1 - self.growth_rate ** self.amount) / (1 - self.growth_rate)
-        diff_cost = total_cost - previous_cost
+        total_cost = self.growth_rate * (1 - self.growth_rate ** (n + self.amount-1)) / (1 - self.growth_rate)
+        previous_cost = self.growth_rate * (1 - self.growth_rate ** (self.amount-1)) / (1 - self.growth_rate)
+        diff_cost = self.base_cost * (total_cost - previous_cost)
         return diff_cost
 
     def unlock_index(self):
-        index = 0
+        index = -1
         for threshold in self.unlock_thresholds:
             if self.amount >= threshold:
                 index = index + 1
@@ -255,12 +211,16 @@ class Resource:
         return index
 
     def next_unlock_threshold(self):
-        return self.unlock_thresholds[self.unlock_index() + 1]
+        unlock_index = self.unlock_index()
+        if unlock_index + 1 < len(self.unlock_thresholds):
+            return self.unlock_thresholds[self.unlock_index() + 1]
+        else:
+            return 1e6
 
     def num_to_purchase(self):
         to_next_threshold = self.next_unlock_threshold() - self.amount
         base_increase = math.floor(self.amount*(purchase_factor - 1))
-        return min(base_increase, to_next_threshold)
+        return max(1, min(base_increase, to_next_threshold))
 
     def increment_if_purchased(self):
         bought_resource = copy.copy(self)
@@ -276,7 +236,7 @@ class UpgradeManager:
     def __init__(self):
         self.current_index = 0
 
-        #
+        # Cost to achieve an upgrade. Start at 0 and then go up (0 is for current/initial cost)
         self.upgrade_costs = list()
 
         # Cumulative factor, start at 1, then multiples all the way up.
@@ -289,16 +249,113 @@ class UpgradeManager:
         return self.upgrade_factors[self.current_index]
 
     def next_cost(self):
-        return self.upgrade_costs[self.current_index + 1]
+        if self.current_index + 1 < len(self.upgrade_costs):
+            return self.upgrade_costs[self.current_index + 1]
+        else:
+            return math.inf
 
     def next_factor(self):
-        return self.upgrade_factors[self.current_index + 1]
+        if self.current_index + 1 < len(self.upgrade_factors):
+            return self.upgrade_factors[self.current_index + 1]
+        else:
+            return 0
 
     def make_purchase(self):
         self.current_index += 1
 
 
+class StatTracker:
 
-DataLoader().load_prod_resources()
+    def __init__(self):
+        self.demand = list()  # type: List[float]
+        self.production = list()  # type: List[float]
+        self.income = list()
+        self.delta_time = list()  # type: List[float]
+        self.time = list()  # type: List[float]
+        self.prod_counts = list()  # type: List[List[int]]
+        self.demand_counts = list()  # type: List[List[int]]
 
-opt = Optimizer()
+    def update_stats(self, optimizer):
+        # type: (Optimizer) -> None
+        self.demand.append(optimizer.total_demand_income())
+        self.production.append(optimizer.total_prod_income())
+        self.income.append(optimizer.total_income())
+        self.update_demand_resource_counts(optimizer)
+        self.update_prod_resource_counts(optimizer)
+
+    def update_demand_resource_counts(self, optimizer):
+        # type: (Optimizer) -> None
+        if len(self.demand_counts) == 0:
+            self.demand_counts = [[] for _ in optimizer.demand_resources]
+        index = 0
+        for r in optimizer.demand_resources:
+            self.demand_counts[index].append(r.amount)
+            index += 1
+
+    def update_prod_resource_counts(self, optimizer):
+        # type: (Optimizer) -> None
+        if len(self.prod_counts) == 0:
+            self.prod_counts = [[] for _ in optimizer.prod_resources]
+        index = 0
+        for r in optimizer.prod_resources:
+            self.prod_counts[index].append(r.amount)
+            index += 1
+
+    def update_time(self, cost, optimizer):
+        # type: (float, Optimizer) -> None
+        time_taken = cost / optimizer.total_income()
+        self.delta_time.append(time_taken)
+        if len(self.time) > 0:
+            self.time.append(self.time[-1] + time_taken)
+        else:
+            self.time.append(time_taken)
+
+    def time_hours(self):
+        return [t/3600 for t in self.time]
+
+
+if __name__ == "__main__":
+    data_loader = DataLoader.DataLoader()
+    data_loader.load_optimizer()
+    opt = data_loader.load_optimizer()
+    opt.run_optimization(1000)
+
+    stat_tracker = opt.stat_tracker
+
+    plt.semilogy(stat_tracker.time_hours(), [d/3600 for d in stat_tracker.delta_time])
+    plt.figure()
+
+    time_hours = stat_tracker.time_hours()
+    index = 0
+    for count_list in stat_tracker.demand_counts:
+        plt.plot(time_hours, count_list, label="Demand Resource {0} Count".format(index))
+        index += 1
+    plt.legend()
+    plt.figure()
+
+    time_hours = stat_tracker.time_hours()
+    index = 0
+    for count_list in stat_tracker.prod_counts:
+        plt.plot(time_hours, count_list, label="Prod Resource {0} Count".format(index))
+        index += 1
+    plt.legend()
+    plt.show()
+
+    power_value = PowerValue()
+    power_value.amount_second_growth_rate = 200
+    power_value.base_cost = 10
+
+    factors = list()
+    costs = list()
+    counts = list()
+    for _ in range(100):
+        factors.append(power_value.factor())
+        costs.append(power_value.cost())
+        counts.append(power_value.count)
+        power_value.make_purchase()
+
+    plt.semilogy(factors)
+    plt.semilogy(costs)
+    # plt.plot(counts)
+    plt.show()
+
